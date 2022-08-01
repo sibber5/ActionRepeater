14532bae6216cbf739fbd756a5d13d4a38487087
cyberrex5 <ghassanjad2004@gmail.com>
2022-08-02 00:02:36 +0300

    Changed publish version to 0.1.0-alpha

--- build_unpackaged_win10-x64.cmd
+++ build_unpackaged_win10-x64.cmd
@@ -7,6 +7,6 @@ for /f "usebackq tokens=*" %%i in (`vswhere.exe -latest -requires Microsoft.Comp
 )
 
 :afterLoop
-%msbuildPath% ActionRepeater.sln -t:restore,rebuild,publish -p:Configuration=Release -p:Platform=x64 -p:Version=0.0.1-alpha -p:SelfContained=False -p:PublishReadyToRun=False -p:SatelliteResourceLanguages=en
+%msbuildPath% ActionRepeater.sln -t:restore,rebuild,publish -p:Configuration=Release -p:Platform=x64 -p:Version=0.1.0-alpha -p:SelfContained=False -p:PublishReadyToRun=False -p:SatelliteResourceLanguages=en
 echo:
 pause
