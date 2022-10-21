2ca8dce88b74ce2bdc0de422769ab1754f5b3c84
cyberrex5 <ghassanjad2004@gmail.com>
2022-10-22 01:06:30 +0300

    Updated build/publish script and csproj

--- build_unpackaged_win10-x64.cmd
+++ build_unpackaged_win10-x64.cmd
@@ -7,6 +7,21 @@ for /f "usebackq tokens=*" %%i in (`vswhere.exe -latest -requires Microsoft.Comp
 )
 
 :afterLoop
-%msbuildPath% ActionRepeater.sln -t:restore,rebuild,publish -p:Configuration=Release -p:Platform=x64 -p:Version=0.1.0-alpha -p:SelfContained=False -p:PublishReadyToRun=False -p:SatelliteResourceLanguages=en
+
+set platform=x64
+set runtimeIdentifier=win10-%platform%
+%msbuildPath% ".\src\ActionRepeater.UI\ActionRepeater.UI.csproj" -t:restore,rebuild,publish -p:Configuration=Release -p:Platform=%platform% -p:RuntimeIdentifier=%runtimeIdentifier% -p:Version=0.1.0-alpha -p:SelfContained=False -p:PublishReadyToRun=False -p:SatelliteResourceLanguages=en
+
+set publishDir=".\src\ActionRepeater.UI\bin\Release"
+set targetPublishDir=".\bin\Release"
+
+echo:
+xcopy %publishDir% %targetPublishDir% /i /s /e /y /k /v /q
+echo Copied publish directory to %targetPublishDir%
+
+echo:
+rmdir %publishDir% /s /q
+echo Deleted old publish directory (%publishDir%)
+
 echo:
 pause
